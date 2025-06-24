889374360cd2397bc6ec1de807952040a0c04627
Matthew An <146902210+mattan0427@users.noreply.github.com>
2025-06-24 19:47:23 -0400

    Update with Comments Euromart Project.sql

--- Euromart Project.sql
+++ Euromart Project.sql
@@ -7,6 +7,7 @@ ADD CONSTRAINT pk_orderid PRIMARY KEY (OrderID)
 
 ALTER TABLE EachOrderBreakdown
 ADD CONSTRAINT fk_orderid FOREIGN KEY (OrderID) REFERENCES OrderList (OrderID)
+--First altering the tables to create a rule where the OrderID is the primary key in OrderList and foreign key in EachOrderBreakdown.
 
 
 --Q2 Split City_State_Country into 3 individual columns namely 'City', 'State', 'Country'.
@@ -16,6 +17,7 @@ ALTER TABLE OrderList
 ADD City nvarchar(50),
 	State nvarchar(50),
 	Country nvarchar(50)
+--Altering table to add three new columns.
 
 UPDATE OrderList
 SET Country = PARSENAME(REPLACE(City_State_Country, ',', '.'), 1), 
@@ -24,7 +26,7 @@ SET Country = PARSENAME(REPLACE(City_State_Country, ',', '.'), 1),
 
 ALTER TABLE OrderList
 DROP Column City_State_Country
-
+--Parsing the City_State_Country column to separate the city, state, and country and adding each to the three new columns.
 
 
 --Q3 Add a new Category Column using the following mapping as per the first 3 characters in the ProductName Column
@@ -41,11 +43,13 @@ SET Category = CASE WHEN LEFT(ProductName, 3) = 'OFS' Then 'Office Supplies'
 					WHEN LEFT(ProductName, 3) = 'TEC' Then 'Technology'
 					WHEN LEFT(ProductName, 3) = 'FUR' Then 'Furniture'
 				END
+--Adding another column named Category and using the first three characters in ProductName to determine which category they fit in.
 
 
 --Q4 Extract the characters after the hyphen from the PorductName Column.
 UPDATE EachOrderBreakdown
 SET ProductName = RIGHT(ProductName, LEN(ProductName) - CHARINDEX('-', ProductName))
+--Deleting the first few characters in ProductName before the hyphen.
 
 
 --Q5 Remove duplicate rows from EachOrderBreakdown table, if all column values are matching.
@@ -53,7 +57,7 @@ SELECT *, ROW_NUMBER() OVER (PARTITION BY OrderID, ProductName, Discount, Sales,
 								Category ORDER BY OrderID) AS rn
 FROM EachOrderBreakdown
 WHERE OrderID = 'AZ-2011-6674300'
-
+--First checking one OrderID to see if it has duplicates.
 
 WITH CTE_duplicate AS(
 SELECT *, ROW_NUMBER() OVER (PARTITION BY OrderID, ProductName, Discount, Sales, Profit, Quantity, SubCategory,
@@ -62,6 +66,9 @@ FROM EachOrderBreakdown
 )
 DELETE FROM CTE_duplicate
 WHERE rn > 1
+--Using a windows function to label each row partioned by each column.
+--Creating a CTE and deleting every row that is greater than 1 in the rn column.
+--This gets rid of all duplicate rows.
 
 
 --Q6 Replace Blank with NA in OrderPriority Column in OrdersList table.
@@ -70,3 +77,4 @@ SELECT * FROM OrderList
 UPDATE OrderList
 SET OrderPriority = 'NA'
 WHERE OrderPriority IS NULL;
+--This updates the OrderList table to replace and row that is Null with NA in the OrderPriority column.
